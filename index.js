985db2264b4c39e9f12851efec9a4c2b0f04c054
sasaplus1 <sasaplus1@gmail.com>
2016-07-04 11:55:56 +0900

    improved

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var db, dd;
 function get() {
   var result;
 
-  db || (db = document.body);
-  dd || (dd = document.documentElement);
-
   if (window.pageYOffset !== void 0) {
     result = window.pageYOffset;
   } else {
+    db || (db = document.body);
+    dd || (dd = document.documentElement);
+
     result = dd.scrollTop || db.parentNode.scrollTop || db.scrollTop;
   }
 
